f32609ac50118dbb19ef35820e159d981cfd6e4b
sreyaarc <sreya66nambiar@gmail.com>
2023-05-29 21:16:13 +0530

    modify endpoints in requests.js

--- src/requests.js
+++ src/requests.js
@@ -2,17 +2,19 @@ const apiKey = process.env.REACT_APP_API_KEY;
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${apiKey}&language=en-US`,
-    fetchNetflixOriginals: `discover/tv?api_key=${apiKey}&with_networks=213`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${apiKey}&with_networks=213`,
+    // fetchNetflixOriginals: `/network/213?api_key=${apiKey}&language=en-US`,
     fetchTopRated: `/movie/top_rated?api_key=${apiKey}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${apiKey}&with_genres=28`,
-    fetchAdventureMovies: `/discover/movie?api_key=${apiKey}&with_genres=17`,
+    fetchAdventureMovies: `/discover/movie?api_key=${apiKey}&with_genres=12`,
     fetchComedyMovies: `/discover/movie?api_key=${apiKey}&with_genres=35`,
     fetchHorrorMovies: `/discover/movie?api_key=${apiKey}&with_genres=27`,
     fetchAnimationMovies: `/discover/movie?api_key=${apiKey}&with_genres=16`,
     fetchMysteryMovies: `/discover/movie?api_key=${apiKey}&with_genres=9648`,
     fetchRomanceMovies: `/discover/movie?api_key=${apiKey}&with_genres=10749`,
     fetchDramaMovies: `/discover/movie?api_key=${apiKey}&with_genres=18`,
-    fetchDocumentaryMovies: `/discover/movie?api_key=${apiKey}&with_genres=99`
+    fetchDocumentaryMovies: `/discover/movie?api_key=${apiKey}&with_genres=99`,
+    fetchTVShows: `/discover/tv?api_key=${apiKey}&language=en-US`
 }
 
 
