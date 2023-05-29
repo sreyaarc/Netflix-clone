30b4cb523c3afffe91680a7cf658e8c3fac6ec08
sreyaarc <sreya66nambiar@gmail.com>
2023-05-29 12:48:16 +0530

    modify axios.js

--- src/axios.js
+++ src/axios.js
@@ -4,4 +4,6 @@ const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3"
 })
 
+// instance.get("/newPage") --> this will append this piece of url to the base url
+// and the url becomes "https://api.themoviedb.org/3/newPage"
 export default instance;
