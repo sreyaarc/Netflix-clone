ba0e3ffd97b862a128770fd6642f27bf408ed0ad
sreyaarc <sreya66nambiar@gmail.com>
2023-06-01 21:15:29 +0530

    create footer component

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import "./footer.css";
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <img src="../Images/netflix-footer.jpg" className="footerImg" alt="footerImage" />
+            <span>&#169; Netflix | 2023</span>
+        </div>
+    )
+}
